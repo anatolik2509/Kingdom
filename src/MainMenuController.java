0e703982db04fbdcef392c5ae98eef5ab3e05a1a
anatolik2509 <antonov250901@mail.ru>
2017-12-28 15:36:07 +0300

    Initial commit

---
+++ src/MainMenuController.java
@@ -0,0 +1,14 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+
+public class MainMenuController {
+    @FXML
+    FlowPane SecondMenu;
+    @FXML
+    Button GameButton;
+    @FXML
+    Button SettingButton;
+    @FXML
+    Button ExitButton;
+}
